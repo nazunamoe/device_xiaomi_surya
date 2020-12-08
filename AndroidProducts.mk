b4d022c48601d46a8891c61b0b2ce6de961ea495
nazunamoe <nakashimayuki@kakao.com>
2020-12-08 17:07:01 +0900

    surya: Blissify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/ancient_surya.mk
+    $(LOCAL_DIR)/bliss_surya.mk
 
 COMMON_LUNCH_CHOICES := \
-    ancient_surya-user \
-    ancient_surya-userdebug \
-    ancient_surya-eng
+    bliss_surya-user \
+    bliss_surya-userdebug \
+    bliss_surya-eng
--- bliss_surya.mk
+++ bliss_surya.mk
@@ -10,29 +10,22 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
-# Inherit some common Ancient OS stuff.
-$(call inherit-product, vendor/ancient/config/common_full_phone.mk)
+# Inherit some common Bliss stuff.
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
-# Gapps
-TARGET_GAPPS_ARCH := arm64
-IS_PHONE := true
-
-# Bootanimation Resolution
+# Bootanimation res
 TARGET_BOOT_ANIMATION_RES := 1080
-
-# Some Extra's
-ANCIENT_OFFICIAL=false
+BLISS_BUILDTYPE=UNOFFICIAL
 
 # Inherit Gapps
 $(call inherit-product-if-exists, vendor/gapps/config.mk)
 TARGET_GAPPS_ARCH := arm64
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := ancient_surya
+PRODUCT_NAME := bliss_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO X3
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-

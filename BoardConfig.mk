fb9b33689e1555b11cd1f7a23800db7d5a77042e
nazunamoe <nakashimayuki@kakao.com>
2020-12-10 22:07:47 +0900

    defconfig: Use nanahira kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -94,7 +94,7 @@ BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 TARGET_KERNEL_ARCH := arm64
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 ifeq ($(TARGET_PREBUILT_KERNEL),)
-  TARGET_KERNEL_CONFIG := surya_defconfig
+  TARGET_KERNEL_CONFIG := vendor/nanahira_defconfig
   TARGET_KERNEL_CLANG_COMPILE := true
   TARGET_KERNEL_SOURCE := kernel/xiaomi/surya
 endif

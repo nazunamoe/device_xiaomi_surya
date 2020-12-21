bca56b132e39cce9d036eb55f80734a0e2135456
nazunamoe <nakashimayuki@kakao.com>
2020-12-21 21:28:49 +0900

    surya: nusantara

--- nad_surya.mk
+++ nad_surya.mk
@@ -11,18 +11,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
 # Inherit some common Bliss stuff.
-$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
+$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
 
 # Bootanimation res
 TARGET_BOOT_ANIMATION_RES := 1080
-BLISS_BUILDTYPE=UNOFFICIAL
+TARGET_INCLUDE_WIFI_EXT := true
+NAD_BUILD_TYPE := UNOFFICIAL
 
 # Inherit Gapps
 $(call inherit-product-if-exists, vendor/gapps/config.mk)
 TARGET_GAPPS_ARCH := arm64
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := bliss_surya
+PRODUCT_NAME := nad_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO X3
